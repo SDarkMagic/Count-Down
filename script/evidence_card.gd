ab59a73c7678ecdb19f5e63fa79c35c69a8cd305
Mo <mouseychu@gmail.com>
2026-07-24 21:29:07 -0400

    drag and drop working
    drag and drop working, beginning to work on moving button with drag

--- script/evidence_card.gd
+++ script/evidence_card.gd
@@ -1,6 +1,6 @@
 class_name EvidenceCard
 extends Button
-
+signal is_dragged(EvidenceCard)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,9 +11,22 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	# set nodes position to be mouse position? 
+	position = get_viewport().get_mouse_position()
+	#print(get_viewport().get_mouse_position())
 	pass
 
 func _get_drag_data(at_position: Vector2) -> Variant:
 	#print(self.text)
+	is_dragged.emit(self)
+	print(at_position)
+	#set_drag_preview(self)
 	return self
 	
+func _on_evidence_card_is_dragged(data: EvidenceCard) -> void:
+	print("signal received")
+	
+	#var drag_data := data as EvidenceCard
+	#label.text = drag_data.text
+	#print("signal funct: ", text)
+	pass # Replace with function body.
